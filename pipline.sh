fbf6290401970bab1f069a67059bf182f34b3110
Andromeda1101 <zhenyu-w23@mails.tsinghua.edu.cn>
2025-06-20 01:36:51 +0800

    update gitignore

--- pipline.sh
+++ pipline.sh
@@ -1,4 +1,5 @@
-bash launch/train_skeleton.sh
-bash launch/train_skin.sh
-bash launch/predict_skeleton.sh
-bash launch/predict_skin.sh
+# python train_skeleton.py --train_data_list data/train_list.txt --val_data_list data/val_list.txt --data_root data --model_name pct2 --output_dir output/skeleton
+# python predict_skeleton.py --predict_data_list data/test_list.txt --data_root data --model_name pct2 --pretrained_model output/skeleton/best_model.pkl --predict_output_dir predict --batch_size 16
+python train_skin.py --train_data_list data/train_list.txt --val_data_list data/val_list.txt --data_root data --model_name enhanced --output_dir output/skin
+python predict_skin.py --predict_data_list data/test_list.txt --data_root data --model_name enhanced --pretrained_model output/skin/best_model.pkl --predict_output_dir predict --batch_size 16
+
